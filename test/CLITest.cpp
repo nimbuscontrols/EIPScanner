52720e2395268dd0f936fc5af5760a95bce16f35
Aleksey Timin <atimin@gmail.com>
2019-11-18 10:09:44 +0500

    add writing to example

--- test/CLITest.cpp
+++ test/CLITest.cpp
@@ -20,6 +20,8 @@ int main() {
 	auto si = std::make_shared<SessionInfo>("127.0.0.1", 0xAF12);
 
 	MessageRouter messageRouter(si);
+
+	// Read attribute
 	auto response = messageRouter
 			.sendRequest(ServiceCodes::GET_ATTRIBUTE_SINGLE,
 						 EPath(0x01, 1, 1),
@@ -31,6 +33,32 @@ int main() {
 		buffer >> vendorId;
 
 		Logger(LogLevel::INFO) << "Vendor ID is " << vendorId;
+	} else {
+		Logger(LogLevel::ERROR) << "We got error=0x" << std::hex << response.getGeneralStatusCode();
+	}
+
+	//Write attribute
+	// See OpenEr EDS 160 line
+	Buffer assembly151;
+	assembly151 << CipUsint(1)
+				<< CipUsint(2)
+				<< CipUsint(3)
+				<< CipUsint(4)
+				<< CipUsint(5)
+				<< CipUsint(6)
+				<< CipUsint(7)
+				<< CipUsint(8)
+				<< CipUsint(9)
+				<< CipUsint(10);
+
+
+	response = messageRouter
+			.sendRequest(ServiceCodes::SET_ATTRIBUTE_SINGLE,
+						 EPath(0x04, 151, 3),
+						 assembly151.data());
+
+	if (response.getGeneralStatusCode() == GeneralStatusCodes::SUCCESS) {
+		Logger(LogLevel::INFO) << "Writing is successful";
 	} else {
 		Logger(LogLevel::ERROR) << "We got error=0x" << std::hex << response.getGeneralStatusCode();
 	}
