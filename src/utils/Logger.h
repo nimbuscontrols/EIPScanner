116f8bee32f0dbaeb75555dc315272df4db1e46c
Aleksey Timin <atimin@gmail.com>
2019-12-15 22:13:42 +0500

    EIPScanner-12: document util classes

--- src/utils/Logger.h
+++ src/utils/Logger.h
@@ -20,6 +20,12 @@ namespace utils {
 		TRACE
 	};
 
+	/**
+	 * @class LogAppenderIf
+	 *
+	 * @brief Interface to print message in the logger
+	 * @sa Logger
+	 */
 	class LogAppenderIf {
 	public:
 		using UPtr = std::unique_ptr<LogAppenderIf>;
@@ -28,6 +34,11 @@ namespace utils {
 		virtual void print(const std::string& msg) = 0;
 	};
 
+	/**
+	 * @class ConsoleAppender
+	 *
+	 * @brief Implements out log messages to std::cout
+	 */
 	class ConsoleAppender : public LogAppenderIf {
 	public:
 		using UPtr = std::unique_ptr<LogAppenderIf>;
@@ -35,19 +46,47 @@ namespace utils {
 	};
 
 
+	/**
+	 * @class
+	 *
+	 * @brief Implement logging
+	 */
 	class Logger {
 	public:
+		/**
+		 * @brief Sets the lowest log level for all log messages
+		 * @note to set off all logs use LogLevel::OFF
+		 * @param level
+		 */
 		static void setLogLevel(LogLevel level);
+
+		/**
+		 * @brief Sets appender to print messages for all log messages
+		 *
+		 * The default appender is ConsoleAppender
+		 * @param appender
+		 */
 		static void setAppender(LogAppenderIf::UPtr appender);
 
 		Logger(LogLevel level);
 
+		/**
+		 * @brief Add message to the log
+		 * @tparam T type of the data to print
+		 * @param msg The message to print
+		 * @return
+		 */
 		template <typename T>
 		std::ostringstream& operator << (T msg) {
 			_stream << msg;
 			return _stream;
 		}
 
+		/**
+		 * @brief Default destructor
+		 *
+		 * The destructor prints all messages, that were added by << operator, before the logger are destroyed
+		 */
 		~Logger();
 
 	private:
